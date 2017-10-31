c563a84f344ee52681609f1fa330859b2b1e11be
Zubkov <penekdedok@gmail.com>
2017-11-01 02:46:15 +0400

    lab3 task 1

--- src/PO52.Zubkov.wdad/data/managers/PreferencesManager.java
+++ src/PO52.Zubkov.wdad/data/managers/PreferencesManager.java
@@ -1,4 +1,6 @@
 package PO52.Zubkov.wdad.data.managers;
+
+import PO52.Zubkov.wdad.utils.PreferencesConstantManager;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -12,93 +14,148 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
+import java.util.Enumeration;
+import java.util.Properties;
 
 public class PreferencesManager {
     private static PreferencesManager instance;
-        private Document doc;
-        private static final String FILE_PATH="src\\PO52\\Zubkov\\wdad\\resources\\configuration\\appconfig.xml";
+    private Document doc;
+    private static final String FILE_PATH = "src\\PO52\\Zubkov\\wdad\\resources\\configuration\\appconfig.xml";
 
-                private PreferencesManager() throws Exception {
-               DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-                factory.setValidating(false);
-                DocumentBuilder builder = factory.newDocumentBuilder();
-               doc = builder.parse(new File(FILE_PATH));
-            }
-
-                public static PreferencesManager getInstance() throws Exception {
-               if (instance == null)
-                        instance = new PreferencesManager();
-                return instance;
-           }
-
-                private Element getElement(String nameField) {
-        NodeList nodeList = doc.getElementsByTagName(nameField);
-               Element element = (Element) nodeList.item(0);
-              return element;
-            }
-
-                public String getCreateregistry() {
-                return getElement("createregistry").getTextContent();
-            }
-
-                public void setCreateregistry(String value) {
-                getElement("createregistry").setTextContent(value);
-            }
-
-                public String getRegistryaddress() {
-               return getElement("registryaddress").getTextContent();
-           }
-
-                public void setRegistryaddress(String value) {
-                getElement("registryaddress").setTextContent(value);
-            }
-
-                public int getRegistryport() {
-               return Integer.parseInt(getElement("registryport").getTextContent());
-            }
-
-                public void setRegistryport(int value) {
-                getElement("registryport").setTextContent(String.valueOf(value));
-           }
-
-               public String getPolicypath() {
-                return getElement("policypath").getTextContent();
-            }
-
-                public void setPolicypath(String value) {
-                getElement("policypath").setTextContent(value);
-           }
-
-                public String getUsecodebaseonly() {
-                return getElement("usecodebaseonly").getTextContent();
-            }
-
-                public void setUsecodebaseonly(String value) {
-                getElement("usecodebaseonly").setTextContent(value);
-           }
-
-                public String getClassprovider() {
-                return getElement("classprovider").getTextContent();
-            }
-
-                public void setClassprovider(String value) {
-                getElement("classprovider").setTextContent(value);
-            }
-
-                public void saveTransformXml()
-
-        {
-                try {
-                        Transformer transformer = TransformerFactory.newInstance()
-                                        .newTransformer();
-                       DOMSource source = new DOMSource(doc);
-                        StreamResult result = new StreamResult(new File(FILE_PATH));
-                        transformer.transform(source, result);
-                   } catch (TransformerConfigurationException ex) {
-                        System.out.println(ex.getMessage());
-                    } catch (TransformerException ex) {
-                        System.out.println(ex.getMessage());
-                    }
-            }
+    private PreferencesManager() throws Exception {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setValidating(false);
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        doc = builder.parse(new File(FILE_PATH));
     }
+
+    public static PreferencesManager getInstance() throws Exception {
+        if (instance == null)
+            instance = new PreferencesManager();
+        return instance;
+    }
+
+    private Element getElement(String nameField) {
+        NodeList nodeList = doc.getElementsByTagName(nameField);
+        Element element = (Element) nodeList.item(0);
+        return element;
+    }
+
+    public void setProperty(String key, String value) {
+        getElement(key).setTextContent(value);
+    }
+
+    public String getProperty(String key) {
+        return getElement(key).getTextContent();
+    }
+
+    public void setProperties(Properties prop) {
+        Enumeration elements = prop.elements();
+        Enumeration keys = prop.keys();
+        while (elements.hasMoreElements()) {
+            getElement((String) elements.nextElement()).setTextContent((String) keys.nextElement());
+        }
+
+    }
+
+    public Properties getProperties() {
+        Properties prop = new Properties();
+        String[] keys = new String[]{
+                PreferencesConstantManager.CREATEREGISTRY,
+                PreferencesConstantManager.REGISTRYADDRESS,
+                PreferencesConstantManager.REGISTRYPORT,
+                PreferencesConstantManager.POLICYPATH,
+                PreferencesConstantManager.USECODEBASEONLY,
+                PreferencesConstantManager.CLASSPROVIDER
+        };
+
+        for (String key : keys) {
+            setProperty(key, getElement(key).getTextContent());
+        }
+
+        return prop;
+    }
+
+    public void addBindedObject(String name, String className) {
+        Element element = (Element) doc.createElement("bindedobject");
+        element.setAttribute("class", className);
+        element.setAttribute("name", name);
+        getElement("rmi").appendChild(element);
+    }
+
+    public void removeBindedObject(String name) {
+        NodeList nodeList = doc.getElementsByTagName("bindedobject");
+        Element element;
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            element = (Element) nodeList.item(i);
+            if (element.getAttribute("name").equals(name)) {
+                getElement("rmi").removeChild(element);
+            }
+        }
+    }
+    @Deprecated
+    public String getCreateregistry() {
+        return getElement("createregistry").getTextContent();
+    }
+    @Deprecated
+    public void setCreateregistry(String value) {
+        getElement("createregistry").setTextContent(value);
+    }
+    @Deprecated
+    public String getRegistryaddress() {
+        return getElement("registryaddress").getTextContent();
+    }
+    @Deprecated
+    public void setRegistryaddress(String value) {
+        getElement("registryaddress").setTextContent(value);
+    }
+    @Deprecated
+    public int getRegistryport() {
+        return Integer.parseInt(getElement("registryport").getTextContent());
+    }
+    @Deprecated
+    public void setRegistryport(int value) {
+        getElement("registryport").setTextContent(String.valueOf(value));
+    }
+    @Deprecated
+    public String getPolicypath() {
+        return getElement("policypath").getTextContent();
+    }
+    @Deprecated
+    public void setPolicypath(String value) {
+        getElement("policypath").setTextContent(value);
+    }
+    @Deprecated
+    public String getUsecodebaseonly() {
+        return getElement("usecodebaseonly").getTextContent();
+    }
+    @Deprecated
+    public void setUsecodebaseonly(String value) {
+        getElement("usecodebaseonly").setTextContent(value);
+    }
+    @Deprecated
+    public String getClassprovider() {
+        return getElement("classprovider").getTextContent();
+    }
+    @Deprecated
+    public void setClassprovider(String value) {
+        getElement("classprovider").setTextContent(value);
+    }
+
+    public void saveTransformXml()
+
+    {
+        try {
+            Transformer transformer = TransformerFactory.newInstance()
+                    .newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File(FILE_PATH));
+            transformer.transform(source, result);
+        } catch (TransformerConfigurationException ex) {
+            System.out.println(ex.getMessage());
+        } catch (TransformerException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
 
